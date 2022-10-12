7c6158215649b1b5e3aa6b120a2a5f5c6b80d323
Martín <magadiflo@gmail.com>
2022-10-12 13:58:45 -0500

    Agregando información a la implementación de Bill Pugh Singleton

--- src/main/java/app/singleton/holder/Singleton.java
+++ src/main/java/app/singleton/holder/Singleton.java
@@ -1,8 +1,19 @@
 package app.singleton.holder;
 
 /**
- * A Bill Pugh se le ocurrió un enfoque diferente para crear la clase Singleton
- * utilizando una clase auxiliar estática interna como se muestra a continuación.
+ * Implementación de Bill Pugh Singleton
+ * *************************************
+ * Antes de Java 5, el modelo de memoria java tenía muchos problemas y los enfoques anteriores
+ * solían fallar en ciertos escenarios donde demasiados subprocesos intentan obtener la instancia
+ * de la clase Singleton simultáneamente. Así que a Bill Pugh se le ocurrió un enfoque diferente
+ * para crear la clase Singleton utilizando una clase auxiliar estática interna.
+ *
+ * Observe la clase estática interna privada que contiene la instancia de la clase singleton.
+ * Cuando se carga la clase singleton, la clase no se carga en la memoria y solo cuando alguien
+ * llama al método getInstance, esta clase se carga y crea la instancia de clase Singleton.
+ * Este es el enfoque más utilizado para la clase Singleton, ya que no requiere sincronización.
+ *
+ * Estoy usando este enfoque en muchos de mis proyectos y también es fácil de entender e implementar.
  */
 
 public class Singleton {
@@ -11,8 +22,8 @@ public class Singleton {
         System.out.println("Creating...");
     }
 
-    static class Holder {
-        static final Singleton INSTANCE = new Singleton(); //lazy
+    private static class Holder {
+        private static final Singleton INSTANCE = new Singleton(); //lazy
     }
 
     public static Singleton getInstance() {
